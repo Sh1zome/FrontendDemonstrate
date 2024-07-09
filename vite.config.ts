6442bf7f1b5bafd651f08da642320db4ba8d1f69
Nelka11 <elizavetamironova1109@gmail.com>
2024-07-10 02:58:06 +0500

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,14 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './',
   plugins: [react()],
+  resolve: {
+    alias: {
+      process: "process/browser",
+      stream: "stream-browserify",
+      zlib: "browserify-zlib",
+      util: 'util',
+    },
+  },
 })
